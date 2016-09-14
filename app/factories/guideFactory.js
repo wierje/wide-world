446e447a11ee5a2bf0b1491c2d3eb3a0e5244925
wierje <wier.je@gmail.com>
2016-09-14 13:26:09 -0500

    additional files added

---
+++ app/factories/guideFactory.js
@@ -0,0 +1,20 @@
+// 'use strict';
+
+// app.factory('GuideFactory', ($q, $http) => {
+//   let getGuides = function() {
+//     let books = [];
+//     return $q((resolve, reject) => {
+//       $http.get('../../data/guides.json')
+//       .success((bookObject) => {
+//         resolve (bookObject);
+//         console.log('bookObject');
+//       })
+//       .error(error);
+//       reject(error);
+//     });
+//   };
+// return{getGuides};
+// console.log(bookObject);
+// });
+
+// // /data/guides.json
